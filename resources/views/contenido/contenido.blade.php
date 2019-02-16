bd10d9f5fca52b99361f4649c7b92c1c6c8310d3
thmxluis <gonzalez.g.luis.m@gmail.com>
2019-02-16 16:55:12 -0500

    Base del Proyecto

---
+++ resources/views/contenido/contenido.blade.php
@@ -0,0 +1,55 @@
+   @extends('principal')
+   @section('contenido')
+       <template v-if="menu==0">
+            <example-component></example-component>
+       </template>
+
+       <template v-if="menu==1">
+            <h1>Contenido 1</h1>
+       </template>
+
+       <template v-if="menu==2">
+            <h1>Contenido 2</h1>
+       </template>
+
+       <template v-if="menu==3">
+            <h1>Contenido 3</h1>
+       </template>
+
+       <template v-if="menu==4">
+            <h1>Contenido 4</h1>
+       </template>
+
+       <template v-if="menu==5">
+            <h1>Contenido 5</h1>
+       </template>
+
+       <template v-if="menu==6">
+            <h1>Contenido 6</h1>
+       </template>
+
+       <template v-if="menu==7">
+            <h1>Contenido 7</h1>
+       </template>
+
+       <template v-if="menu==8">
+            <h1>Contenido 8</h1>
+       </template>
+
+       <template v-if="menu==9">
+            <h1>Contenido 9</h1>
+       </template>
+
+       <template v-if="menu==10">
+            <h1>Contenido 10</h1>
+       </template>
+
+       <template v-if="menu==11">
+            <h1>Contenido 11</h1>
+       </template>
+
+       <template v-if="menu==12">
+            <h1>Contenido 12</h1>
+       </template>
+
+   @endsection
